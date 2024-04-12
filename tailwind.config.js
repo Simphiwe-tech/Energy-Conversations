f973a5a01c086db9b3151f86f0853bad501c7f2e
Simphiwe-tech <113884198+Simphiwe-tech@users.noreply.github.com>
2024-04-12 15:25:45 +0200

    Energy Convos first upload

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
+const rotateY = plugin(function ({ addUtilities }) {
+  addUtilities ({
+    '.rotate-y-180': {
+      transform: "rotateY(180deg)"
+    },
+    '.-rotate-y-180': {
+      transform: "rotateY(-180deg)"
+    }
+  })
+})
+
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "color-primary": "#f8f8ff",
+        "color-primary-light": "#332c49",
+        "color-primary-dark": "#958AB6",
+        "color-secondary": "#CEA754",
+        "color-tertiary": "#4814E4",
+        "color-gray": "#808080",
+        "color-white": "#FFF8EE",
+        "color-blob": "#000000",
+      }
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '20px',
+        md: "50px"
+      }
+    }
+  },
+  plugins: [rotateY],
+}
